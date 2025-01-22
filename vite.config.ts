0c94d86d54708e0b7d4cc33cd3d2def0ebbcf613
divyam-zluri <divyam.s@zluri.com>
2025-01-22 12:04:31 +0530

    minor change

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import dotenv from 'dotenv'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  define: {
-    'process.env': JSON.stringify(dotenv.config().parsed),
-  },
 })
